e17161f65c449882d2ca01de1c84829d2f600b91
Teddy Pascual <teddylayson2000@gmail.com>
2023-07-12 16:22:04 +0800

    database connection

---
+++ database.php
@@ -0,0 +1,17 @@
+<?php
+  $db_server = "localhost";
+  $db_user = "root";
+  $db_pass = "pass1234";
+  $db_name = "businessdb";
+  $conn = "";
+  try {
+    $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+  }
+  catch(mysqli_sql_exception) {
+    echo "Could not Connect!";
+  }
+
+  if($conn) {
+    echo "You are connected!";
+  }
+?>
