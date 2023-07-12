8176cfe33c2f5035b8f9c1cc958c697749173781
Teddy Pascual <teddylayson2000@gmail.com>
2023-07-12 11:37:19 +0800

    include lesson and cookies page

---
+++ about.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>About</title>
+</head>
+<body>
+<?php
+  include("header.html");
+?>
+  <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, mollitia eaque architecto libero officiis dicta a sapiente eveniet minima, non voluptatum sed dolor id. Quam repudiandae natus ullam minus praesentium.</h4>
+  <?php
+  include("footer.html");
+?>
+</body>
+</html>
